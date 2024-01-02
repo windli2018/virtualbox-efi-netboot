f8a0444e999f5e859a6f24e970efc911d87c8101
Ben Hildred <hildred@users.noreply.github.com>
2024-01-01 23:10:53 -0700

    auto install packages (#1)

--- build-ipxe-efi-iso.sh
+++ build-ipxe-efi-iso.sh
@@ -1,9 +1,28 @@
-#!/bin/sh
+#!/bin/bash
+
+function install_pkg () {
+	[ -x "$1" ] && return 0
+	if [ -x /usr/bin/apt ] ; then
+		apt install "$2" -y
+	elif [ -x /usr/bin/yum ] ; then
+		yum install "$2" -y
+	else
+		echo please install "$2" before running this script
+		exit 1
+	fi
+	[ -x "$1" ] && return 0
+	echo "$1" is not execcutable
+	exit 1
+}
+
+install_pkg git git
+install_pkg make make
+install_pkg mkisofs genisoimage
+
 git clone git://git.ipxe.org/ipxe.git
 cd ipxe/src/
 make -j4 bin-x86_64-efi/ipxe.efi
 make -j4 bin-x86_64-efi/ipxe.usb
 mkdir iso
 cp -avf bin-x86_64-efi/ipxe.usb iso
-#apt install genisoimage -y || yum install genisoimage -y
 mkisofs -e ipxe.usb -o ipxe.efi.iso iso/
